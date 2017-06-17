47989081a810e5b0e45a9815a5a001c2044f1383
Luis Naranjo <luisnaranjo733@gmail.com>
2017-06-16 17:14:17 -0700

    add db context

---
+++ SoftecData/SoftecData/Models/DataContext.cs
@@ -0,0 +1,14 @@
+﻿    using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoftecData.Models
+{
+    public class DataContext : DbContext
+    {
+        public DbSet<DataItem> DataItems { get; set; }
+    }
+}
