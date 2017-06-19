80d9bb7550a1a692fb2601e188a991ef659b1ed5
Luis Naranjo <luisnaranjo733@gmail.com>
2017-06-19 14:37:23 -0700

    setup up form param processing

--- SoftecWeb/src/core/urls.py
+++ SoftecWeb/src/core/urls.py
@@ -5,4 +5,5 @@ from core import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^data/$', views.provide_data_items, name='provide_data_items')
 ]
--- SoftecWeb/src/core/views.py
+++ SoftecWeb/src/core/views.py
@@ -1,6 +1,32 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from core.models import DataItem
+
+
+from werkzeug.security import generate_password_hash, \
+     check_password_hash
+
 
 # Create your views here.
 def index(request):
+    'index'
     return HttpResponse('hello, world')
+
+from pprint import pprint
+
+@csrf_exempt
+def provide_data_items(request):
+    'Provide data items'
+    if request.method == 'POST':
+        restaurant_id = request.POST.get('restaurant-id', None)
+        password = request.POST.get('password', '')
+
+        # data_items = DataItem.objects.filter()
+
+    data_items_map = {
+        'key1': 'value1',
+        'key2': 'value2'
+    }
+    return JsonResponse(data_items_map)
+
