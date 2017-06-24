c12f5e506a07863c5a551e41855882f9442dc6bc
Luis Naranjo <luisnaranjo733@gmail.com>
2017-06-23 23:44:54 -0700

    looks like the login algorithm is working

--- SoftecData/SoftecData/Pages/LoginPage.xaml.cs
+++ SoftecData/SoftecData/Pages/LoginPage.xaml.cs
@@ -36,9 +36,62 @@ namespace SoftecData.Pages
             InitializeComponent();
         }
 
+        /// <summary>
+        /// Algorithm:
+        /// AM
+        ///     Month + 2
+        ///     Day + 5
+        /// PM
+        ///     Month + 5
+        ///     Day + 2
+        ///     
+        /// Example:
+        /// 06/21 AM = 0826 
+        /// 06/21 PM = 1123
+        /// 
+        /// 06/23 PM = 1125
+        /// </summary>
+        /// <returns></returns>
+        public static string ComputePassword(DateTime currentDate)
+        {
+           
+
+            int monthModifier;
+            int dayModifier;
+
+            bool isAM = currentDate.ToString("tt") == "AM";
+            if (isAM)
+            {
+                monthModifier = 2;
+                dayModifier = 5;
+            } else
+            {
+                monthModifier = 5;
+                dayModifier = 2;
+            }
+
+            int leftPartInt = currentDate.Month + monthModifier;
+            int rightPartInt = currentDate.Day + dayModifier;
+
+            string leftPartString = leftPartInt.ToString().PadLeft(2, '0');
+            string rightPartString = rightPartInt.ToString().PadLeft(2, '0');
+
+            return leftPartString + rightPartString;
+        }
+
         private void loginBtn_Click(object sender, RoutedEventArgs e)
         {
-            NavigationService.Navigate(new PasswordsPage(_storageService, _accountRepository));
+            string providedPassword = passwordBox.Password;
+            DateTime currentDate = DateTime.Now;
+            if (providedPassword == ComputePassword(currentDate))
+            {
+                NavigationService.Navigate(new PasswordsPage(_storageService, _accountRepository));
+            } else
+            {
+                MessageBox.Show("Wrong password, try again");
+                passwordBox.Clear();
+            }
+            
         }
     }
 }
